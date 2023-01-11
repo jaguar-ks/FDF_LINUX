a8b90b9fd1ed32b836b4d19939e0ed3289adac91
jaguar-ks <fahdamineksouss@gmail.com>
2023-01-12 00:05:29 +0100

    as long as the W_HT = 3/2 W_WT we won't have a problem of centring the map

--- tools/init_point.c
+++ tools/init_point.c
@@ -6,7 +6,7 @@
 /*   By: deman_wolf <deman_wolf@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:37:53 by faksouss          #+#    #+#             */
-/*   Updated: 2023/01/11 23:56:56 by deman_wolf       ###   ########.fr       */
+/*   Updated: 2023/01/12 00:02:00 by deman_wolf       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_pst	start_point(t_inf cr)
 {
 	t_pst	p;
 
-	p.x = (W_HT * 200) / 1500;
+	p.x = (W_HT * 250) / 1500;
 	p.y = W_WT / 2;
 	p.z = cr.z;
 	p.cl = cr.cl;
